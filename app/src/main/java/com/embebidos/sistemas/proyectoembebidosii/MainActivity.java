ad39b824925a824546bf21327cbb161e733bb020
Sergio <Sergio@localhost.localdomain>
2015-02-21 01:45:04 -0600

    Creada primera entrega

---
+++ app/src/main/java/com/embebidos/sistemas/proyectoembebidosii/MainActivity.java
@@ -0,0 +1,52 @@
+package com.embebidos.sistemas.proyectoembebidosii;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class MainActivity extends ActionBarActivity implements OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        View btnAceptar = findViewById(R.id.btnAceptarLogin);
+        btnAceptar.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==findViewById(R.id.btnAceptarLogin).getId()) {
+            Intent i=new Intent(this, Menu_pricipal.class);
+            startActivity(i);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
